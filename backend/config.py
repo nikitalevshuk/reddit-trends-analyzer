3fade12d4cdcbab967a458981a0da849aff7018d
nikitalevshuk <levshukn@gmail.com>
2025-03-20 22:53:24 +0300

    made some fixes for connection between front and back

--- backend/config.py
+++ backend/config.py
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
     # JWT
     JWT_SECRET_KEY: str
     JWT_ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     class Config:
         env_file = ".env"
